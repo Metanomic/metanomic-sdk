fef713081e0d10245865bc2f687874916cb6ee97
Evangelos Pappas <epappas@evalonlabs.com>
2022-07-28 22:39:50 +0100

    upgrade and fixes on entities

--- src/entities/event.ts
+++ src/entities/event.ts
@@ -5,31 +5,31 @@ import { Product } from "./product";
 import { EventType, IDRef, VersionRef } from "./types";
 
 export class EventSchema {
-  type: EventType
-  version: VersionRef
-  eventId: IDRef
-  appId: IDRef
-  userId: IDRef
-  installationId: IDRef
-  playerId: IDRef
-  sessionId: IDRef
+  type!: EventType;
+  version!: VersionRef;
+  eventId!: IDRef;
+  appId!: IDRef;
+  userId!: IDRef;
+  installationId!: IDRef;
+  playerId!: IDRef;
+  sessionId!: IDRef;
 
   doTrack: boolean = true
-  expiresAt: number
-  receivedAt: number
-  sentAt: number
-  timestamp: number
+  expiresAt!: number;
+  receivedAt!: number;
+  sentAt!: number;
+  timestamp!: number;
   eventSinceAppstart: number = 0
 
-  sourceContext: EventSourceContextSchema
+  sourceContext!: EventSourceContextSchema;
 
-  value: EventValue
-  linkedAssets: [Asset]
-  linkedProduct: [Product]
+  value!: EventValue;
+  linkedAssets!: [Asset];
+  linkedProduct!: [Product];
 }
 
 export class RecordSchema {
-  appId: IDRef
-  events: [EventSchema]
+  appId!: IDRef;
+  events!: EventSchema[];
 }
 
