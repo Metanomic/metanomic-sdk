a9d8d184846c7e16cacf18845667ffebad44d25d
Evangelos Pappas <epappas@evalonlabs.com>
2022-07-28 22:41:02 +0100

    new config structure

---
+++ src/util/config.ts
@@ -0,0 +1,8 @@
+export const VERSION: string = 'BETA';
+export const BUILD: string = 'LATEST';
+export const NAMESPACE: string = 'Metanomic';
+export const BASE_API_URL: string = 'https://api.metanomic.cloud';
+export const EVENTS_PER_PUSH = 10
+export const BUFFER_INTERVAL = 1000
+export const BUFFER_TIME_GAP = 150
+
